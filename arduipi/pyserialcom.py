6edebd7e9ca5bc757c9cb3cadd9e872db0583657
Eric <ericyx.zhang@mail.utoronto.ca>
2015-01-12 01:15:38 -0500

    alamaganated jobs created

--- arduipi/pyserialcom.py
+++ arduipi/pyserialcom.py
@@ -62,6 +62,58 @@ def arduiinit():
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout = 0.1)
     time.sleep(1)
 
+
+'''
+Colour function: changecolor
+Inputs: 
+DeviceID: a four digit devide id to send the command to 
+Red: the colour of red to display, [0,255] in string 
+Green: the colour of green to display, [0,255] in string
+Blue: the colour of blue to display, [0,255] in string
+
+Functionality: 
+this function makes use of the three functions of 
+
+Output: The output of this function is 0 if all commands are received 
+and 1 if one of the messages failed 
+
+Edge Cases
+
+'''
+def changecolor(deviceID, Red, Green, Blue):
+    red = deviceID + '014' + Red
+    green = deviceID + '014' + Green
+    blue = deviceID + '014' + Blue
+
+    send_red = arduisend(red)
+    send_green = arduisend(green)
+    send_blue arduisend(blue)
+
+    if send_red == 1 and send_green == 1 and send_blue == 1: 
+        return 0 
+    else: 
+        print ('colours not sent')
+        return 1 
+
+''' 
+Sensor Poll function: pollsensor
+this function sends a message and returns the sensor information from 
+the arduino sensor 
+
+Inputs: None 
+
+Output: the sensor information 
+if failed, returns the value read from serial port
+
+'''
+def pollsensor(deviceID)
+    mssage = deviceID + '013000'
+    arduisend('mssage')
+
+    val = arduireceive()
+    return val 
+
+
 # global variable declaration
 serp = 0 
 ser = 0 
